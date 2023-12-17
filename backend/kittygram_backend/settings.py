4df5b341f24643b10f9801645254042dd4a33668
Георгий Болезин <g.bolezin@zenith24.ru>
2023-12-17 13:10:06 +0700

    Added photo storgae

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -101,7 +101,8 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
