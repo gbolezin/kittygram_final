681041f841be1912fe78432a986c158f78e95100
Георгий Болезин <g.bolezin@zenith24.ru>
2023-12-19 00:26:59 +0700

    Change STATIC_DIR

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
-# SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+
 DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS', '[]')).split(',')
@@ -102,8 +102,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/backend_static/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static' 
+STATIC_ROOT = '/backend_static/static/'
+# STATIC_ROOT = BASE_DIR / 'collected_static' 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = BASE_DIR / 'media'
