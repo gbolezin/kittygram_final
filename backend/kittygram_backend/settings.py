6f8bb9f80dcb70747d026a02f11567ce2e2462bd
Георгий Болезин <g.bolezin@zenith24.ru>
2023-12-18 22:21:57 +0700

    Restore working parameters of Django

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,19 +1,21 @@
 # flake8: noqa
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
 
-load_dotenv()
+# load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+# SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+# DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = True
 
-DEBUG = os.getenv('DEBUG')
-
-ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS')).split(', ')
+# ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS', '[]')).split(', ')
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -102,8 +104,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/backend_static/static/'
-
+# STATIC_ROOT = '/backend_static/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static' 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = BASE_DIR / 'media'
