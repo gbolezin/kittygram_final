129a343ac884f5f0a40ec41f26bf4773ee7d07f6
Георгий Болезин <g.bolezin@zenith24.ru>
2023-12-18 22:47:26 +0700

    Add env Debug option

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,18 +1,18 @@
 # flake8: noqa
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
 
-# load_dotenv()
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-# DEBUG = os.getenv('DEBUG', 'False')
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False')
+# DEBUG = True
 
 # ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS', '[]')).split(', ')
 ALLOWED_HOSTS = []
