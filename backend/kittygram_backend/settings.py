f3321fd081b1de6b2e666499f35dcb18dcbe49aa
Георгий Болезин <g.bolezin@zenith24.ru>
2023-12-19 00:51:05 +0700

    Change collect_static

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -101,7 +101,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/backend_static/static/'
+STATIC_ROOT = '/backend_static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media'
 
