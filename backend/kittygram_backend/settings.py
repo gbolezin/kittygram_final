d568c7115781d630350aaa3986f28780fe8f3b8f
Георгий Болезин <g.bolezin@zenith24.ru>
2023-12-18 12:00:44 +0700

    Second commit after review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,11 +3,13 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG')
 
